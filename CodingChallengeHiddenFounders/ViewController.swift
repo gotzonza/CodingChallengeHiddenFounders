65e8a6f7d8611e40558b4a8d42d249e7d40503f8
Gotzon Zabala <gotzon@Mac-mini-de-Gotzon.local>
2018-01-28 20:44:26 +0100

    Almost all done. UITableView with all the specs done.
    To do:
    		- size classes. Set constraints to be available on all the devices.
    		- pagination.

--- CodingChallengeHiddenFounders/ViewController.swift
+++ CodingChallengeHiddenFounders/ViewController.swift
@@ -17,14 +17,14 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     var repoDescription = [String]()
     var ownerImageUrl = [String]()
     var ownerName = [String]()
-    var totalStars = [Int]()
+    var totalStars = [Float]()
 
+    
    
     struct GitHubDescription: Decodable {
-     //   let total_count: Int
         let items: [GitHub]
-        
     }
+    
     struct Owner: Decodable {
         let login: String
         let avatar_url: String
@@ -32,21 +32,9 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
     struct GitHub: Decodable {
         let name: String
-      //  let description: String
-       // let login: String
         let owner: Owner
         let description: String?
-        let stargazers_count: Int
-        
-        /*
-        init(json: [String: Any]) {
-            repoName = json["name"] as? String ?? ""
-            repoDescription = json["description"] as? String ?? ""
-            ownerName = json["tipsy"] as? String ?? ""
-            numberStars = json["stargazers_count"] as? Int ?? -1
-            
-        }
- */
+        let stargazers_count: Float
     }
     
   
@@ -54,15 +42,25 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        apiTask()
     
         
-        let jsonUrl = "https://api.github.com/search/repositories?q=created:%3E2017-10-22&sort=stars&order=desc"
+       
+    }
+    
+  
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func apiTask() {
+        
+        let jsonUrl = "https://api.github.com/search/repositories?q=created:%3E2017-10-22&sort=stars&order=desc&page=1"
         guard let url = URL(string: jsonUrl) else { return }
         
         URLSession.shared.dataTask(with: url) { (data, response, error) in
-            
-            //Check error
-            //Check response status
             
             guard let data = data else { return }
             
@@ -70,36 +68,29 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             do {
                 
                 let repos = try JSONDecoder().decode(GitHubDescription.self, from: data)
-            
                 
-              
-                print(repos.items[0])
-                
-                
-                
-                
-             //   self.repoDescription = repos.items[0].description as? String ?? ""
-              
-                
-            
                 DispatchQueue.main.sync {
                     
                     self.repositories = repos.items.count - 1
                     
-                    for x in 0...self.repositories{
+                    for x in 0...self.repositories{     //to enter data on the arrays
+                        
                         self.repoName.append(repos.items[x].name)
-                        if repos.items[x].description == nil{
-                            self.repoDescription.append("")
-                        }else{
-                            self.repoDescription.append(repos.items[x].description!)
-                        }
+                        
+                            if repos.items[x].description == nil{
+                                self.repoDescription.append("")
+                            }else{
+                                self.repoDescription.append(repos.items[x].description!)
+                            }
+                        
+                        self.ownerImageUrl.append(repos.items[x].owner.avatar_url)
                         self.ownerName.append(repos.items[x].owner.login)
                         self.totalStars.append(repos.items[x].stargazers_count)
-               
+                        
                     }
-                     self.reposTableOutlet.reloadData()
+                    self.reposTableOutlet.reloadData()
                 }
-               
+                
                 
             } catch let jsonError {
                 
@@ -112,13 +103,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             }.resume()
     }
     
-  
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
+    //MARK: TableView
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
@@ -131,16 +116,69 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         let cellTable = tableView.dequeueReusableCell(withIdentifier: "RepoCellTable", for: indexPath) as! RepoTableViewCell
         
         let index = indexPath.row
-        
+    
         cellTable.repoName.text = repoName[index]
         cellTable.repoDescription.text = repoDescription[index]
-     //   cellTable.ownerImage
+        cellTable.ownerImage.downloadedFrom(link: ownerImageUrl[index])
         cellTable.ownerName.text = ownerName[index]
-        cellTable.totalStars.text = "\(totalStars[index])"
+        
+        if totalStars[index] > 1000 {
+        
+            let stars = totalStars[index] / 1000
+
+            cellTable.totalStars.text = "\(String(NSString(format: "%.01f", stars)))k"
+        }else{
+            cellTable.totalStars.text = "\(Int(totalStars[index]))"
+        }
+        
         
         return cellTable
     }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        
+        let frame: CGRect = tableView.frame
+        
+        let headerView: UIView = UIView(frame: CGRect(x: 0, y: 0, width: frame.size.width, height: frame.size.height))
+        
+        let textHeader: UILabel = UILabel(frame: CGRect(x: 0, y: 10, width: view.frame.width, height: 50))
+        if ownerName.isEmpty == false {
+            textHeader.text = "Trending Repos"
+            headerView.backgroundColor = UIColor.gray
+        }
+        
+        textHeader.textAlignment = NSTextAlignment.center
 
+        
+        headerView.addSubview(textHeader)
+        
+        return headerView
+    }
 
+   
+
+}
+
+//This is an extension to show the image from a url.
+
+extension UIImageView {
+    func downloadedFrom(url: URL, contentMode mode: UIViewContentMode = .scaleAspectFit) {
+        contentMode = mode
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard
+                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
+                let data = data, error == nil,
+                let image = UIImage(data: data)
+                else { return }
+            DispatchQueue.main.async() {
+                self.image = image
+            }
+            }.resume()
+    }
+    func downloadedFrom(link: String, contentMode mode: UIViewContentMode = .scaleAspectFit) {
+        guard let url = URL(string: link) else { return }
+        downloadedFrom(url: url, contentMode: mode)
+    }
 }
 
